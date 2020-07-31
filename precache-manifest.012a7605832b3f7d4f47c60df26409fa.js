1125a7e96f3368b59e5208005028d36b62c80beb
tanoto <tanoto@outlook.com>
2020-07-30 22:08:56 -0400

    Updates

---
+++ precache-manifest.012a7605832b3f7d4f47c60df26409fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17e6ed7cfeb2bf57a375a2e700ab481",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "3ac8d64e2b442755da72",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9db970cb986d5474660",
+    "url": "/react-app/static/js/2.f908b780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.f908b780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac8d64e2b442755da72",
+    "url": "/react-app/static/js/main.6e7cd054.chunk.js"
+  },
+  {
+    "revision": "ac872109be052ea36539",
+    "url": "/react-app/static/js/runtime-main.bdfca5b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
